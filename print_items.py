8ab85067f66e97b6b462160f3c7f12a9859f13e5
BurneyProMod <38512280+BurneyProMod@users.noreply.github.com>
2021-08-23 13:23:01 -0500

    Updated print_items.py to comply with Python 3.0

--- print_items.py
+++ print_items.py
@@ -1,17 +1,15 @@
 from bs4 import BeautifulSoup
 import os
 
+
 for filename in os.listdir('guides'):
-    print '------' + filename + '------'
+    print('------', filename, '------')
     with open('guides/' + filename) as f:
         raw = f.read()
-
     try:
         soup = BeautifulSoup(raw, "html.parser")
-
         table = soup.findAll("table", {"class": "questdetails"})[0]
         tds = table.findAll("td", {"class": "questdetails-info"})
-
-        print "Items Required", tds[5].get_text()
+        print('Items Required', tds[5].get_text())
     except Exception:
-        print "Failed :("       
+        print('Failed')
